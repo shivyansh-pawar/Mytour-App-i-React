b529ffda9af5973c0b14aeb9a6915eee10196ddb
shivyansh-pawar <shivyanshbhagat95@gmail.com>
2023-12-25 12:49:54 +0530

    add all files

---
+++ src/data.js
@@ -0,0 +1,55 @@
+const data = [
+    {
+        id: 1,
+        name: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image: "images/agra.jpg",
+        price: "10000",
+    },
+    {
+        id: 2,
+        name: "Darjiling",
+        info: "Darjeeling is a town in India's West Bengal state, in the Himalayan foothills. Once a summer resort for the British Raj elite, it remains the terminus of the narrow-gauge Darjeeling Himalayan Railway, or “Toy Train,” completed in 1881. It's famed for the distinctive black tea grown on plantations that dot its surrounding slopes. Its backdrop is Mt. Kanchenjunga, among the world’s highest peaks.",
+        image: "images/darjiling.jpg",
+        price: "5346",
+    },
+    {
+        id: 3,
+        name: "Goa",
+        info: "Goa is a state in western India with coastlines stretching along the Arabian Sea. Its long history as a Portuguese colony prior to 1961 is evident in its preserved 17th-century churches and the area’s tropical spice plantations.",
+        image: "images/goa.jpg",
+        price: "76755",
+    },
+    {
+        id: 4,
+        name: "Jaipur",
+        info: "Jaipur is the capital of India’s Rajasthan state. It evokes the royal family that once ruled the region and that, in 1727, founded what is now called the Old City, or “Pink City” for its trademark building color.",
+        image: "images/jaipur.jpg",
+        price: "3543",
+    },
+    {
+        id: 5,
+        name: "Jaisalmer",
+        info: "Jaisalmer is a former medieval trading center and a princely state in the western Indian state of Rajasthan, in the heart of the Thar Desert. Known as the Golden City,it's distinguished by its yellow sandstone architecture. Dominating the skyline is Jaisalmer Fort, a sprawling hilltop citadel buttressed by 99 bastions.",
+        image: "images/jaisalmer.jpg", 
+        price: "4357",
+    },
+    {
+        id: 6,
+        name: "Kochi",
+        info: "Kochi (also known as Cochin) is a city in southwest India's coastal Kerala state. It has been a port since 1341, when a flood carved out its harbor and opened it to Arab, Chinese and European merchants. Sites reflecting those influences include Fort Kochi, a settlement with tiled colonial bungalows and diverse houses of worship.",
+        image: "images/kochi.jpg",
+        price: "45646",
+    },
+    {
+        id: 7,
+        name: "Vsranasi",
+        info: "Varanasi is a city in the northern Indian state of Uttar Pradesh dating to the 11th century B.C. Regarded as the spiritual capital of India, the city draws Hindu pilgrims who bathe in the Ganges River’s sacred waters and perform funeral rites.",
+        image: "images/varanasi.jpg",
+        price: "53642",
+    },
+
+   
+];
+
+export default data;
